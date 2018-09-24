f6de9b052206adc877b7b793e8b9f089593d3b0f
macdonald <me@mycomdotcom.com>
2018-09-24 15:37:57 -0400

    test that an error is raised when a WordsearchSolver tries to solve a wordsearch that is not valid

--- solver.py
+++ solver.py
@@ -3,3 +3,8 @@ from wordsearch import *
 class WordsearchSolver:
     def __init__(self, wordsearch):
         self.wordsearch = wordsearch
+        
+    def solve(self):
+        if not self.wordsearch.isValid():
+            raise ValueError("Wordsearch must return True for isValid() to be able to solve")
+        return
--- test_solver.py
+++ test_solver.py
@@ -5,14 +5,22 @@ from solver import *
 
 class WordsearchSolverTests(unittest.TestCase):
         
-    #results should be an empty dictionary that will later be filled with the solutions. 
-    #The solutions to the word puzzle consists of the words as keys and lists of ordered paris as values    
+    #test that wordsearchsolve can store a wordsearch object successfully  
     def test_wordsearchsolver_object_stores_wordsearch_object(self):
         wsearch = Wordsearch("wordsearchEx1.txt")
         wsSolver = WordsearchSolver(wsearch)
         self.assertEqual(wsSolver.wordsearch, wsearch)
 
-        
+    #test that wordsearchsolver will raise a ValueError when it tries to solve a wordsearch that is not valid
+    def test_wordsearchsolver_raises_error_if_it_tries_to_solve_an_invalid_wordsearch(self):
+        wsearch = Wordsearch("wordsearchEx1.txt")
+        wsearch.keywords = [] #make sure the wordsearch will not be considered valid (keywords can't be empty list)
+        wsSolver = WordsearchSolver(wsearch)
+        try:
+            wsSolver.solve()
+        except ValueError:
+            return
+        self.fail()
     
 if __name__ == "__main__":
     unittest.main()
