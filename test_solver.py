64c49129c0b78c9e8ca2517d51f41490c70d42be
macdonald <me@mycomdotcom.com>
2018-10-01 15:35:00 -0400

    Refactored test_solver.py to clean up some duplicate code

--- test_solver.py
+++ test_solver.py
@@ -5,6 +5,10 @@ from solver import *
 
 class WordsearchSolverTests(unittest.TestCase):
         
+    def setUp(self):
+        self.wsearch = Wordsearch("wordsearchEx1.txt")
+        self.wsSolver = WordsearchSolver(self.wsearch)
+        
     #test that wordsearchsolve can store a wordsearch object successfully  
     def test_wordsearchsolver_object_stores_wordsearch_object(self):
         wsearch = Wordsearch("wordsearchEx1.txt")
@@ -13,26 +17,23 @@ class WordsearchSolverTests(unittest.TestCase):
 
     #test that wordsearchsolver will raise a ValueError when it tries to solve a wordsearch that is not valid
     def test_wordsearchsolver_raises_error_if_it_tries_to_solve_an_invalid_wordsearch(self):
-        wsearch = Wordsearch("wordsearchEx1.txt")
-        wsearch.keywords = [] #make sure the wordsearch will not be considered valid (keywords can't be empty list)
-        wsSolver = WordsearchSolver(wsearch)
+        self.wsearch.keywords = [] #make sure the wordsearch will not be considered valid (keywords can't be empty list)
+        wsSolver = WordsearchSolver(self.wsearch)
         try:
             wsSolver.solve()
         except ValueError:
             return
         self.fail()
-        
+    
+    #tests that wordsearchsolver can find a word by searching horizontal to the right and returns the correct list of the letter coordinates
     def test_wordsearchsolve_search_horizontal_right(self):
-        wsearch = Wordsearch("wordsearchEx1.txt")
-        wsSolver = WordsearchSolver(wsearch)
-        result = wsSolver.search(keyword="SCOTTY", direction="RIGHT")
+        result = self.wsSolver.search(keyword="SCOTTY", direction="RIGHT")
         knownResult = [(0,5),(1,5),(2,5),(3,5),(4,5),(5,5)]
         self.assertEqual(result, knownResult)
         
+    #tests that wordsearchsolver can find a word by searching horizontal to the left and returns the correct list of the letter coordinates
     def test_wordsearchsolve_wearch_horizontal_left(self):
-        wsearch = Wordsearch("wordsearchEx1.txt")
-        wsSolver = WordsearchSolver(wsearch)
-        result = wsSolver.search(keyword="KIRK", direction="LEFT")
+        result = self.wsSolver.search(keyword="KIRK", direction="LEFT")
         knownResult = [(4,7),(3,7),(2,7),(1,7)]
         self.assertEqual(result, knownResult)
     
