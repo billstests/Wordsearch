dd66668e293ff03d6377e0520bc14e505bfa4e40
macdonald <me@mycomdotcom.com>
2018-10-02 21:16:58 -0400

    basic word search example as well as benchmarking method

--- example.py
+++ example.py
@@ -1,11 +1,42 @@
 from wordsearch import *
 from solver import *
+from timeit import default_timer as timer
+import sys
 
-#create wordsearch object (here using an external text file with keywords and letter grid)
-wsearch = Wordsearch("wordsearchEx1.txt")
+#returns the average execution time for solving a wordsearch
+def measureSolveExecutionTime(fileLoc = "wordsearchEx1.txt", useFastSearch = False, NumOfTries = 1000):
+    wsearch = Wordsearch(fileLoc)
+    wsSolver = WordsearchSolver(wsearch)
+    
+    start = timer()
+    for i in range(NumOfTries):           #Run solve method NumTimes times
+        wsSolver.solve(useFastSearch)
+    avgTimeElapsed = (timer()-start)/NumOfTries   #calculate average time elaped per run
+    return avgTimeElapsed
 
-#create WordsearchSolver object that takes in a word search
-wsSolver = WordsearchSolver(wsearch)
+#a simple example of how to use the wordsearch and solver objects to print out a wordsearch solution
+def DoExampleWordsearch():
+    #create wordsearch object (here using an external text file with keywords and letter grid)
+    wsearch = Wordsearch("wordsearchEx1.txt")
+    
+    #create WordsearchSolver object that takes in a word search
+    wsSolver = WordsearchSolver(wsearch)
+    
+    #solve and print solution
+    wsSolver.printSolution()
+#compares the fastSolve to the regular solve method for a wordsearch
+def CompareSolveTimes(fileLoc = "wordsearchEx1.txt", NumOfTries = 1000):
+    tFast = measureSolveExecutionTime(fileLoc, True, NumOfTries)
+    tRegular = measureSolveExecutionTime(fileLoc, False, NumOfTries)
+    print(fileLoc + ": RegularSearch_Time = " + str(round(tRegular/tFast,3)) + " x FastSearch_Time")
 
-#solve and print solution
-wsSolver.printSolution()
+if __name__=="__main__":
+    args = sys.argv
+    #default behavior: do a simple wordsearch example
+    if len(args)==1:
+        DoExampleWordsearch()
+    #timetrial option
+    if len(args)==2:
+        if args[1].upper() == "TIMETRIAL":
+            CompareSolveTimes("wordsearchEx1.txt")  #regular = 57xfastSolver on my computer
+            CompareSolveTimes("wordsearchEx2.txt")  #regular = 146xfastSolver on my computer
